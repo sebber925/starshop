bcf432134a30083f8808a301f52838b53b748958
sebber925 <63114738+sebber925@users.noreply.github.com>
2025-04-02 07:10:57 +0200

    Added StarshipApiController and Starship model

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Starship;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipApiController extends AbstractController
+{
+    #[Route('/api/starships')]
+    public function getCollection(): Response
+    {
+        $starships = [
+            new Starship(
+                1,
+                'Millennium Falcon',
+                'Garden',
+                'Jean-Luc Pickles',
+                'under construction',
+            ),
+            new Starship(
+                2,
+                'USS Enterprise',
+                'Starship',
+                'Jean-Luc Picard',
+                'in service',
+            ),
+            new Starship(
+                3,
+                'TARDIS',
+                'Time Machine',
+                'The Doctor',
+                'in service',
+            ),
+        ];
+
+        return $this->json($starships);
+    }
+}
