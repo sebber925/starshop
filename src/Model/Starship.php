14424d94c354f301cf0deb7d26234614e52c77f6
Sebastiaan Mooy <63114738+sebber925@users.noreply.github.com>
2025-04-03 16:40:46 +0200

    Made design dynamic

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -12,24 +12,43 @@ class Starship
         private StarshipStatusEnum $status,
     ) {
     }
+
     public function getId(): int
     {
         return $this->id;
     }
+
     public function getName(): string
     {
         return $this->name;
     }
+
     public function getClass(): string
     {
         return $this->class;
     }
+
     public function getCaptain(): string
     {
         return $this->captain;
     }
+
     public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }
+
+    public function getStatusString(): string
+    {
+        return $this->status->value;
+    }
+
+    public function getStatusImageFilename(): string
+    {
+        return match ($this->status) {
+            StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+            StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+            StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+        };
+     }
 }
